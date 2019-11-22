ccc19ebc25271847c814375da64e611329a60964
idesantiagocontreras <idesantiagocontreras@gmail.com>
2019-11-22 17:56:21 -0600

    seccion 4

---
+++ sockets/socket.ts
@@ -0,0 +1,16 @@
+import { Socket } from 'socket.io';
+import socketIO from 'socket.io';
+
+export const desconectar = (cliente: Socket) => {
+    
+    cliente.on('disconnect', () =>{
+        console.log('Cliente desconectado');
+    });
+}
+
+export const mensaje = (cliente: Socket, io: socketIO.Server) => {
+    cliente.on('mensaje', ( payload ) =>{
+        console.log('Mensaje Recibido:', payload);
+        io.emit('mensaje-nuevo', payload);
+    });
+}
